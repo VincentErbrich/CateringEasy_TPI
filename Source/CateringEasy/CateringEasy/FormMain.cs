52a43d4810cf5741d9d801c41f6d1fea5165454d
VincentErbrich <vincent.erbrich@cpnv.ch>
2018-05-18 15:04:53 +0200

    Travail d'analyse et de conception du 18.05.2018
    Eléments principaux:
    MCD
    Stratégie de tests
    Environnement de développement

---
+++ Source/CateringEasy/CateringEasy/FormMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CateringEasy
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            CarteRestaurant crt = new CarteRestaurant();
+        }
+    }
+}
